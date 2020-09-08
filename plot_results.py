12d81534f993357ff02b7f3e791a12dd98228485
Matteo Belenchia <matteobel20@ru.is>
2020-09-08 14:36:50 +0000

    moved charts to appropriate folder; generalized plot script; added boxcharts and individual charts for each algorithm

--- plot_results.py
+++ plot_results.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+charts_folder = 'charts/'
+
 name_of_run = "easy_arc"
 
 output_file = "output_" + name_of_run + ".txt"
+
+length_of_run = 50
 
 # This probably is not the best way to do read the file but it works
 df = pd.read_csv(output_file, delim_whitespace=True, header=None)
@@ -13,24 +17,57 @@ for i in range(4, 85):
     df = df.drop(columns=i)
 
 # T
-plt.bar(list(range(50)), df[2].iloc[0:50], label='BT')
-plt.bar(list(range(50)), df[2].iloc[50:100], label='BJ')
-plt.bar(list(range(50)), df[2].iloc[100:150], label='CBJ')
+plt.bar(list(range(length_of_run)), df[2].iloc[0:length_of_run], label='BT')
+plt.bar(list(range(length_of_run)), df[2].iloc[length_of_run:2*length_of_run], label='BJ')
+plt.bar(list(range(length_of_run)), df[2].iloc[2*length_of_run:3*length_of_run], label='CBJ')
 
 plt.xlabel('Sudoku No.')
 plt.ylabel('# nodes')
 plt.legend()
 plt.tight_layout()
-plt.savefig(name_of_run + '_nodes')
+plt.savefig(charts_folder+name_of_run + '_nodes')
 
 plt.clf()
 
-plt.bar(list(range(50)), df[3].iloc[0:50], label='BT')
-plt.bar(list(range(50)), df[3].iloc[50:100], label='BJ')
-plt.bar(list(range(50)), df[3].iloc[100:150], label='CBJ')
+plt.bar(list(range(length_of_run)), df[3].iloc[0:length_of_run], label='BT')
+plt.bar(list(range(length_of_run)), df[3].iloc[length_of_run:2*length_of_run], label='BJ')
+plt.bar(list(range(length_of_run)), df[3].iloc[2*length_of_run:3*length_of_run], label='CBJ')
 
 plt.xlabel('Sudoku No.')
 plt.ylabel('Time (s)')
 plt.legend()
 plt.tight_layout()
-plt.savefig(name_of_run + '_time')
+plt.savefig(charts_folder+name_of_run + '_time')
+
+
+# charts for individual algorithms
+
+for j, t in enumerate(['nodes', 'time']):
+    for i, v in enumerate(['BT', 'BJ', 'CBJ']):
+        plt.clf()
+        plt.bar(list(range(length_of_run)), df[j + 2].iloc[0 + (i * length_of_run):length_of_run + (i * length_of_run)], label=v)
+        plt.xlabel('Sudoku No.')
+        ylabel = '# nodes' if j == 0 else 'Time (s)'
+        plt.ylabel(ylabel)
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(charts_folder+name_of_run + '_' + t + '_' + v)
+
+
+#boxplots
+
+plt.clf()
+plt.boxplot([df[2].iloc[0:length_of_run], df[2].iloc[length_of_run:2*length_of_run],
+             df[2].iloc[2*length_of_run:3*length_of_run]], labels=["BT", "BJ", "CBJ"])
+plt.xlabel('Algorithm')
+plt.ylabel('# nodes')
+plt.tight_layout()
+plt.savefig(charts_folder+name_of_run + '_nodes_boxplot')
+
+plt.clf()
+plt.boxplot([df[3].iloc[0:length_of_run], df[3].iloc[length_of_run:2*length_of_run],
+             df[3].iloc[2*length_of_run:3*length_of_run]], labels=["BT", "BJ", "CBJ"])
+plt.xlabel('Algorithm')
+plt.ylabel('Time (s)')
+plt.tight_layout()
+plt.savefig(charts_folder+name_of_run + '_time_boxplot')
